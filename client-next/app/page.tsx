616be26d88e0cc5e75feda87f70c722e54c967f1
kise <kaiazmi1412@gmail.com>
2026-09-23 18:47:22 +0530

    fix: sm responsive
    - width full on small screen.
    - middle and large screen responsive.

--- client-next/app/page.tsx
+++ client-next/app/page.tsx
@@ -11,7 +11,7 @@ async function getJobs() {
 export default async function Page() {
   const jobs = await getJobs();
   return (
-    <div className="max-w-5xl mx-auto bg-neutral-100 dark:bg-neutral-50">
+    <div className="md:max-w-3xl lg:max-w-5xl w-full md:mx-auto bg-neutral-100 dark:bg-neutral-50">
       <Jobs initialJobs={jobs} />
     </div>
   );
